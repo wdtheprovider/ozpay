8cab81d843d03eceffb88f27c0141e426d142db9
Dingaan <velly.dingaan17@gmail.com>
2022-06-01 15:41:39 +0200

    c

--- config.php
+++ config.php
@@ -3,32 +3,24 @@
 //starting the session
 session_start();
 
-// base url
-define('BASE_URL', 'http://localhost/ozpay/');
+// base url - Change to your website - for mine i'm using a https://ding.icu/ozpay 
+define('BASE_URL', 'http://localhost/ozpay/'); // use this if you are testing it on localhost with xampp
 
 //Look for this values in your dashboard account on https://dash.ozow.com/
-
-/*
-$siteCode = 'WDT-WDT-001';
-$privateKey = 'HzHiOfumUq0bDRDugxEV5tHEtrEwYXeK';
-$ApiKey = 'TyHlLf46efVRiHvqy0neaLleaRQogGsR';
-*/
-
-
-$siteCode = 'TSTSTE0001';
+$siteCode = 'TSTSTE0001'; // change this to your real sitcode and the rest below
 $privateKey = '215114531AFF7134A94C88CEEA48E';
 $ApiKey = 'EB5758F2C3B4DF3FF4F2669D5FF5B';
 
-///
+/// controll the testing, true for testing and false for production.
 $IsTest = 'false';
 
 
-class DbConnect{
+class DbConnect{ //Update this to your database details.
 
     private $servername = 'localhost';
-    private $dbname = 'ozpay';
-    private $username = 'root';
-    private $password = '';
+    private $dbname = 'ozpay'; //database name
+    private $username = 'root'; // database username
+    private $password = ''; // database password, leave it empty if you are using xampp
     public function connect( ){
         try {
             $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
